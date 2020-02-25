41d03b02d66441a476fc35deaa8abecc167fd94f
Mehdi Javan <32511762+mehdi-vaadin@users.noreply.github.com>
2020-02-25 11:20:05 +0200

    Create .travis.script.sh

---
+++ .travis.script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# TRAVIS_PULL_REQUEST == "false" for a normal branch commit, the PR number for a PR
+# TRAVIS_BRANCH == target of normal commit or target of PR
+# TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
+# TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
+# SKIP_SONAR == skip sonar checks
+
+# Exclude the bower_components and node_modules from Sonar analysis
+
+mvn -B -e -V clean verify
